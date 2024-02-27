e2f22f69fe32efccc4c4e79125dbe1f09d352c7a
Abdullah Bin Shahbaz <154223491+abdullahsh7890@users.noreply.github.com>
2024-02-27 20:16:31 +0500

    Add files via upload

---
+++ Javascript Chp 3/app.js
@@ -0,0 +1,17 @@
+//Q1//
+var age = "I am 18 years old"
+alert(age)
+
+//Q2//
+var customerVisit = "You have visited this site 20 times"
+alert(customerVisit)
+
+//Q3//
+var birthYear = "My birth year is 2007 <br/> Data type of my decleared variable is number <br/>"
+document.write(birthYear)
+
+//Q4//
+var VisitorName= " Abdullah "
+var productTitle = " On XYZ Clothing store"
+var quantity = "\n Ordered 5 T-shirt(s) "
+document.write( VisitorName + quantity+productTitle)
